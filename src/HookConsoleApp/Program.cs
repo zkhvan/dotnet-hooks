455d06a17d88e7c3a6d6048fe8cc9b6b820ce2b0
Zhenya Khvan <zhenya.khvan@gmail.com>
2019-08-02 09:36:17 -0400

    chore: create console application project

---
+++ src/HookConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HookConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
